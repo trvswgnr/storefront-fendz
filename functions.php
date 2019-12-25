dba82b7fe249218a47b1b833df44d16356263854
trav <trvswgnr@gmail.com>
2019-12-25 03:25:33 -0500

    enqueue admin styles

--- functions.php
+++ functions.php
@@ -4,3 +4,23 @@
  *
  * @package fendz
  */
+
+// requires.
+require_once 'blocks/class-block.php';
+require_once 'blocks/banner/class-banner.php';
+
+$banner = new Banner();
+
+/**
+ * Admin styles
+ */
+function fendz_admin_styles() {
+	wp_enqueue_style(
+		'admin-styles',
+		get_stylesheet_directory_uri() . '/style.css',
+		array(),
+		'1.0.0',
+		'all'
+	);
+}
+add_action( 'admin_head', 'fendz_admin_styles' );
