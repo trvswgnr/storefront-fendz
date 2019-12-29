71f73303bf7269a81c6d539f26a01f8025b3bccb
trav <trvswgnr@gmail.com>
2019-12-29 02:46:10 -0500

    add footer-lp

---
+++ footer-lp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package storefront
+ */
+
+?>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
