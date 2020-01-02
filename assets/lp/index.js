8e3795d74b04b1ae56c9f9b7132c56daec550466
trav <trvswgnr@gmail.com>
2020-01-02 01:08:26 -0500

    scroll to anchor, sp -> lp

--- assets/lp/index.js
+++ assets/lp/index.js
@@ -1,6 +1,6 @@
 ( function( $ ) {
-	var target = '.sp__cover-cont';
-	var addClass = 'sp__cover--sticky';
+	var target = '.lp__cover-cont';
+	var addClass = 'lp__cover--sticky';
 	var top = $( target ).offset().top;
 	$( window ).scroll( function() {
 		var y = $( this ).scrollTop();
@@ -10,4 +10,22 @@
 			$( target ).removeClass( addClass );
 		}
 	} );
+
+	$( '.js-scroll-anchor' ).click( function( event ) {
+		event.preventDefault();
+		$( 'html, body' ).animate( { scrollTop: $( $( this ).attr( 'href' ) ).offset().top }, 200 );
+	} );
+
+	( function() {
+		var words = $( '.shrinking-text' ).text().split( ' ' );
+		$( '.shrinking-text' ).empty();
+		$.each( words, function( i, v ) {
+			var style, el;
+			style = 'font-size: calc(1em - ' + ( i / ( words.length / 13 ) ) + 'px);';
+			style += 'opacity: ' + ( 1 - ( ( i / ( words.length / 13 ) ) * 0.1 ) ) + ';';
+			el = $( '<span style="' + style + '">' ).text( v + ' ' );
+			$( '.shrinking-text' ).append( el );
+		} );
+	}() );
 }( jQuery ) );
+
