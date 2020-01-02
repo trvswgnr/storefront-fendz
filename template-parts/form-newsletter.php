bc7dd9b7c1469c4db3b782b94e9001ff8ef4a301
trav <trvswgnr@gmail.com>
2020-01-01 21:41:09 -0500

    add newsletter signup

---
+++ template-parts/form-newsletter.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Newsletter Signup Form
+ *
+ * @package fendz
+ */
+
+?>
+<!-- Begin Mailchimp Signup Form -->
+<div id="mc_embed_signup">
+	<form action="https://justfendz.us4.list-manage.com/subscribe/post?u=63cc322912a2be9fa6db7256a&amp;id=948450845a"
+		method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank"
+		novalidate>
+		<div id="mc_embed_signup_scroll">
+			<div class="mc-field-group field-group">
+				<label for="mce-EMAIL">Email</label>
+				<input type="email" value="" name="EMAIL" class="required email input" id="mce-EMAIL">
+			</div>
+			<div class="mc-field-group input-group" style="overflow: hidden;min-height: 0;height: 0;padding: 0;">
+				<strong>Downloads </strong>
+				<ul>
+					<li><input type="checkbox" value="1" name="group[50469][1]" id="mce-group[50469]-50469-0" checked><label for="mce-group[50469]-50469-0">Chonky 808s</label></li>
+				</ul>
+			</div>
+			<div id="mce-responses" class="clear">
+				<div class="response" id="mce-error-response" style="display:none"></div>
+				<div class="response" id="mce-success-response" style="display:none"></div>
+			</div>
+			<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_63cc322912a2be9fa6db7256a_948450845a" tabindex="-1" value=""></div>
+			<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button btn"></div>
+		</div>
+	</form>
+</div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><?php // phpcs:ignore ?>
+<script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+<!--End mc_embed_signup-->
