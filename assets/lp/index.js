fb0ac08f0438cc68fdb675902f336769301b1a9c
trav <trvswgnr@gmail.com>
2020-01-03 00:34:50 -0500

    add rate from data attribute

--- assets/lp/index.js
+++ assets/lp/index.js
@@ -16,16 +16,21 @@
 		$( 'html, body' ).animate( { scrollTop: $( $( this ).attr( 'href' ) ).offset().top }, 200 );
 	} );
 
-	( function() {
-		var words = $( '.shrinking-text' ).text().split( ' ' );
-		$( '.shrinking-text' ).empty();
+	$( '.shrinking-text' ).each( function() {
+		var $this = $( this );
+		var words = $this.text().split( ' ' );
+		var rate = 13;
+		if ( $this.data( 'rate' ) ) {
+			rate = $this.data( 'rate' );
+		}
+		$this.empty();
 		$.each( words, function( i, v ) {
 			var style, el;
-			style = 'font-size: calc(1em - ' + ( i / ( words.length / 13 ) ) + 'px);';
-			style += 'opacity: ' + ( 1 - ( ( i / ( words.length / 13 ) ) * 0.1 ) ) + ';';
+			style = 'font-size: calc(1em - ' + ( i / ( words.length / rate ) ) + 'px);';
+			style += 'opacity: ' + ( 1 - ( ( i / ( words.length / rate ) ) * 0.1 ) ) + ';';
 			el = $( '<span style="' + style + '">' ).text( v + ' ' );
-			$( '.shrinking-text' ).append( el );
+			$this.append( el );
 		} );
-	}() );
+	} );
 }( jQuery ) );
 
