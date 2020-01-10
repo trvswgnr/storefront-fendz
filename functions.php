2db8c55b36bf657c88cda2e1c8906d5ff09e920f
trav <trvswgnr@gmail.com>
2020-01-10 17:46:14 -0500

    add template part function

--- functions.php
+++ functions.php
@@ -9,6 +9,18 @@
 require_once 'blocks/class-block.php';
 require_once 'blocks/banner/class-banner.php';
 
+/**
+ * Helper function to include files from the template-parts folder (allows variables to be passed)
+ * usage: require template_part( 'hero' ); or require template_part( 'hero','large' );
+ *
+ * @param string $part1 First part of module to include (filename before hyphen).
+ * @param string $part2 Second part of module to include (filename after hypen with no extension).
+ */
+function template_part( $part1, $part2 = '' ) {
+	$hyphen = $part2 ? '-' : '';
+	return include locate_template( 'template-parts/' . $part1 . $hyphen . $part2 . '.php' );
+}
+
 $banner = new Banner();
 
 /**
